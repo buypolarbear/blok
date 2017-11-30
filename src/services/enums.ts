35e151ad2a40691029bb95a34a74694f5a1bccfa
IljaDaderko <idaderko@gmail.com>
2017-11-30 11:47:53 +0200

    update enums

--- src/services/enums.ts
+++ src/services/enums.ts
@@ -1,31 +1,31 @@
 const enum DIRECTION {
-  right = 1,
-  left = 2
+  right = "RIGHT",
+  left = "LEFT"
 }
 
 const enum TICKER {
-  BTC = 1,
-  LTC = 2,
-  ETH = 3,
-  XRP = 4,
-  DASH = 5,
-  STEEM = 6
+  BTC = "BTC",
+  LTC = "LTC",
+  ETH = "ETH",
+  XRP = "XRP",
+  DASH = "DASH",
+  STEEM = "STEEM"
 }
 
 const enum SIZE {
-  small = 1,
-  big = 2,
-  normal = 3
+  small = "SMALL",
+  big = "BIG",
+  normal = "NORMAL"
 }
 
 const enum COLOR {
-  white = 1,
-  grey = 2,
-  lightGrey = 3,
-  green = 4,
-  red = 5,
-  darkPurple = 6,
-  darkGrey = 7
+  white = "WHITE",
+  grey = "GREY",
+  lightGrey = "LIGHTGREY",
+  green = "GREEN",
+  red = "RED",
+  darkPurple = "DARKPURPLE",
+  darkGrey = "DARKGREY"
 }
 
 export { DIRECTION, TICKER, SIZE, COLOR };
